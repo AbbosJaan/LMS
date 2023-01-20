48d193125b0a7c47f6b5d1bd99b80bb76dd4f9db
Abbosdev <AbbosAlisherovich2003@gmail.com>
2023-01-20 17:20:08 +0500

    database setting and inforamtionMessages table added

---
+++ LMS.DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LMS.DataAccess.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        DbSet<InformationMessage> InformationMessages { get; set; }
+    }
+}
